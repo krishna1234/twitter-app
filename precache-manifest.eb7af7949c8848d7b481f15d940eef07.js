dd21898011bf3ee99203a1da2bf397f909766f49
ks071866 <ks071866@cerner.net>
2019-11-11 13:10:42 +0530

    Updates

---
+++ precache-manifest.eb7af7949c8848d7b481f15d940eef07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be97171111226526c9218c2ff0fa764",
+    "url": "/krishna1234/twitter-app/index.html"
+  },
+  {
+    "revision": "317244bb08ea5b070b25",
+    "url": "/krishna1234/twitter-app/static/css/main.a440f248.chunk.css"
+  },
+  {
+    "revision": "1fc89f93e2bd6a748583",
+    "url": "/krishna1234/twitter-app/static/js/2.c31a6665.chunk.js"
+  },
+  {
+    "revision": "317244bb08ea5b070b25",
+    "url": "/krishna1234/twitter-app/static/js/main.dab59796.chunk.js"
+  },
+  {
+    "revision": "23a4f2beabb49c2d5488",
+    "url": "/krishna1234/twitter-app/static/js/runtime-main.71eb316a.js"
+  }
+]);
